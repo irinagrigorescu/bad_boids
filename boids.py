94dbb59a2b6c6f1ff317124c6b28c6ed87b794ae
Irina Grigorescu <irinagry@gmail.com>
2016-02-25 15:55:01 +0000

    Replace hand-written code with library code and changed test function to work with new code

--- boids.py
+++ boids.py
@@ -6,6 +6,8 @@ for use as an exercise on refactoring.
 from matplotlib import pyplot as plt
 from matplotlib import animation
 import random
+import numpy as np
+import sys
 
 # Global Parameters
 NO_BOIDS = 50
@@ -14,86 +16,97 @@ ALERT_DISTANCE = 100
 FORMATION_FLYING_DISTANCE = 10000
 FORMATION_FLYING_STRENGTH = 0.125
 
-# Deliberately terrible code for teaching purposes
+# Limits:
+lower_limits_pos = np.array([-450.0, 300.0])
+upper_limits_pos = np.array([  50.0, 600.0])
+lower_limits_vel = np.array([   0.0, -20.0])
+upper_limits_vel = np.array([  10.0,  20.0])
 
-boids_x=[random.uniform(-450,50.0) for x in range(NO_BOIDS)]
-boids_y=[random.uniform(300.0,600.0) for x in range(NO_BOIDS)]
-boid_x_velocities=[random.uniform(0,10.0) for x in range(NO_BOIDS)]
-boid_y_velocities=[random.uniform(-20.0,20.0) for x in range(NO_BOIDS)]
-boids=(boids_x,boids_y,boid_x_velocities,boid_y_velocities)
+#boids_x=[random.uniform(-450,50.0) for x in range(NO_BOIDS)]
+#boids_y=[random.uniform(300.0,600.0) for x in range(NO_BOIDS)]
+#boid_x_velocities=[random.uniform(0,10.0) for x in range(NO_BOIDS)]
+#boid_y_velocities=[random.uniform(-20.0,20.0) for x in range(NO_BOIDS)]
+#boids=(boids_x,boids_y,boid_x_velocities,boid_y_velocities)
+
+# Initialise flock positions and velocities # sunt la schimbarea cu numpy adica refactoring nr 6 hand-written code 
+def new_flock(lower_limits_pos, upper_limits_pos):
+	width = upper_limits_pos - lower_limits_pos
+	return (lower_limits_pos[:, np.newaxis] + np.random.rand(2, NO_BOIDS) * width[:, np.newaxis])
+	
 
 # Fly towards the middle function
-def fly_towards_the_middle(boids):
-	boid_pos_x,boid_pos_y,boid_vel_x,boid_vel_y = boids
+def fly_towards_the_middle(positions, velocities):
 	for i in range(NO_BOIDS):
 		for j in range(NO_BOIDS):
 			attenuation_factor = MOVEMENT_STRENGTH/NO_BOIDS
-			separation_x = boid_pos_x[j] - boid_pos_x[i]
-			separation_y = boid_pos_y[j] - boid_pos_y[i]
-			boid_vel_x[i] = boid_vel_x[i] + separation_x * attenuation_factor
-			boid_vel_y[i] = boid_vel_y[i] + separation_y * attenuation_factor
+			separation_x = positions[0][j] - positions[0][i]
+			separation_y = positions[1][j] - positions[1][i]
+			velocities[0][i] = velocities[0][i] + separation_x * attenuation_factor
+			velocities[1][i] = velocities[1][i] + separation_y * attenuation_factor
 
 # Fly away from nearby boids function
-def fly_away_from_neaby_boids(boids):
-	boid_pos_x,boid_pos_y,boid_vel_x,boid_vel_y = boids
+def fly_away_from_neaby_boids(positions, velocities):
 	for i in range(NO_BOIDS):
 		for j in range(NO_BOIDS):
-			separation_x = boid_pos_x[j] - boid_pos_x[i]
-			separation_y = boid_pos_y[j] - boid_pos_y[i]
+			separation_x = positions[0][j] - positions[0][i]
+			separation_y = positions[1][j] - positions[1][i]
 			squared_displacement_x = separation_x**2
 			squared_displacement_y = separation_y**2
 			square_distances = squared_displacement_x + squared_displacement_y
 			if square_distances < ALERT_DISTANCE:
-				boid_vel_x[i] = boid_vel_x[i] + separation_x
-				boid_vel_y[i] = boid_vel_y[i] + separation_y
+				velocities[0][i] = velocities[0][i] - separation_x
+				velocities[1][i] = velocities[1][i] - separation_y
 				
 # Match speed with nearby boids
-def match_speed_with_nearby_boids(boids):
-	boid_pos_x,boid_pos_y,boid_vel_x,boid_vel_y = boids
+def match_speed_with_nearby_boids(positions, velocities):
 	for i in range(NO_BOIDS):
 		for j in range(NO_BOIDS):
-			separation_x = boid_pos_x[j] - boid_pos_x[i]
-			separation_y = boid_pos_y[j] - boid_pos_y[i]
+			separation_x = positions[0][j] - positions[0][i]
+			separation_y = positions[1][j] - positions[1][i]
 			squared_displacement_x = separation_x**2
 			squared_displacement_y = separation_y**2
-			velocity_difference_x = boid_vel_x[j]-boid_vel_x[i]
-			velocity_difference_y = boid_vel_y[j]-boid_vel_y[i]
+			velocity_difference_x = velocities[0][j] - velocities[0][i]
+			velocity_difference_y = velocities[1][j] - velocities[1][i]
 			square_distances = squared_displacement_x + squared_displacement_y
 			if square_distances < FORMATION_FLYING_DISTANCE:
-				boid_vel_x[i] = boid_vel_x[i] + velocity_difference_x * FORMATION_FLYING_STRENGTH/NO_BOIDS
-				boid_vel_y[i] = boid_vel_y[i] + velocity_difference_y * FORMATION_FLYING_STRENGTH/NO_BOIDS
+				velocities[0][i] = velocities[0][i] + velocity_difference_x * FORMATION_FLYING_STRENGTH/NO_BOIDS
+				velocities[1][i] = velocities[1][i] + velocity_difference_y * FORMATION_FLYING_STRENGTH/NO_BOIDS
 
 # Move according to velocities
-def move_according_to_velocities(boids):
-	boid_pos_x,boid_pos_y,boid_vel_x,boid_vel_y = boids
+def move_according_to_velocities(positions, velocities):
 	for i in range(NO_BOIDS):
-		boid_pos_x[i] = boid_pos_x[i] + boid_vel_x[i]
-		boid_pos_y[i] = boid_pos_y[i] + boid_vel_y[i]
+		positions[0][i] = positions[0][i] + velocities[0][i]
+		positions[1][i] = positions[1][i] + velocities[1][i]
 	
 # Update boids function
-def update_boids(boids):
+def update_boids(positions, velocities):
 	
 	# Fly towards the middle
-	fly_towards_the_middle(boids)
+	fly_towards_the_middle(positions, velocities)
 	
 	# Fly away from nearby boids
-	fly_away_from_neaby_boids(boids)
+	fly_away_from_neaby_boids(positions, velocities)
 	
 	# Try to match speed with nearby boids
-	match_speed_with_nearby_boids(boids)
+	match_speed_with_nearby_boids(positions, velocities)
 	
 	# Move according to velocities
-	move_according_to_velocities(boids)
+	move_according_to_velocities(positions, velocities)
 
+
+# Initialisations
+positions  = new_flock(lower_limits_pos, upper_limits_pos)
+velocities = new_flock(lower_limits_vel, upper_limits_vel)
+#print positions
+#sys.exit(0)
 
 figure=plt.figure()
 axes=plt.axes(xlim=(-500,1500), ylim=(-500,1500))
-scatter=axes.scatter(boids[0],boids[1])
+scatter=axes.scatter(positions[0,:], positions[1,:])
 
 def animate(frame):
-   update_boids(boids)
-   scatter.set_offsets(zip(boids[0],boids[1]))
-
+   update_boids(positions, velocities)
+   scatter.set_offsets(positions.transpose())
 
 anim = animation.FuncAnimation(figure, animate,
                                frames=50, interval=50)
---
+++ test_boids.py
@@ -0,0 +1,19 @@
+from boids import update_boids
+from nose.tools import assert_almost_equal
+import os
+import yaml
+import numpy as np
+
+def test_bad_boids_regression():
+    regression_data = yaml.load(open(os.path.join(os.path.dirname(__file__),'fixture.yml')))
+    boid_data = regression_data["before"]
+
+    positions  = np.append([np.asarray(boid_data[0])], [np.asarray(boid_data[1])], axis=0)
+    velocities = np.append([np.asarray(boid_data[2])], [np.asarray(boid_data[3])], axis=0)
+
+    update_boids(positions, velocities)
+    boid_data_after = np.append(positions, velocities, axis=0) # boid data after update
+    
+    for after,before in zip(regression_data["after"], boid_data_after):
+        for after_value,before_value in zip(after, before): 
+            assert_almost_equal(after_value, before_value, delta=0.01)

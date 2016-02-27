d7e58e50dafa75b18220f3240c0e8da704454a98
Irina Grigorescu <irinagry@gmail.com>
2016-02-27 15:39:43 +0000

    Changed test file to accomodate new folder structure

--- boids/test/test_boids.py
+++ boids/test/test_boids.py
@@ -1,17 +1,17 @@
-import flock as fl
 from nose.tools import assert_almost_equal
 import os
 import yaml
 import numpy as np
+from .. import flock as fl
 
 def test_bad_boids_regression():
-    regression_data = yaml.load(open(os.path.join(os.path.dirname(__file__),'fixture.yml')))
+    regression_data = yaml.load(open(os.path.join(os.path.dirname(__file__),'fixtures/fixture.yml')))
     boid_data = regression_data["before"]
 
     positions  = np.append([np.asarray(boid_data[0])], [np.asarray(boid_data[1])], axis=0)
     velocities = np.append([np.asarray(boid_data[2])], [np.asarray(boid_data[3])], axis=0)
     
-    boids = fl.Flock("config.yaml")
+    boids = fl.Flock("../config.yaml")
     boids.set_positions(positions)
     boids.set_velocities(velocities)
     boids.update_boids()
